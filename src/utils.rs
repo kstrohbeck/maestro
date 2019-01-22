75c4f216429f736d76e692f9e2ca3cb5f184800c
Kyle Strohbeck <kyles@callbox.com>
2019-01-21 18:48:17 -0600

    Add helpers for parsing from yaml hashes.

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,7 @@
 use crate::text::Text;
 use itertools::Itertools;
 use std::cmp::max;
+use yaml_rust::yaml::{Hash, Yaml};
 
 pub fn num_digits(mut number: usize) -> usize {
     let mut count = 0;
@@ -13,6 +14,57 @@ pub fn num_digits(mut number: usize) -> usize {
 
 pub fn comma_separated(text: &[Text]) -> Text {
     text.iter().cloned().intersperse(Text::new(", ")).sum()
+}
+
+pub enum HashDeErr {
+    KeyNotFound,
+    InvalidValue,
+}
+
+pub fn parse_key_from_hash<F, T>(
+    hash: &mut Hash,
+    key: &'static str,
+    transform: &F,
+) -> Result<T, HashDeErr>
+where
+    F: Fn(Yaml) -> Option<T>,
+{
+    let value = pop!(hash[key]).ok_or(HashDeErr::KeyNotFound)?;
+    transform(value).ok_or(HashDeErr::InvalidValue)
+}
+
+pub fn parse_vec_key_from_hash<F, T>(
+    hash: &mut Hash,
+    key: &'static str,
+    transform: &F,
+) -> Result<Vec<T>, HashDeErr>
+where
+    F: Fn(Yaml) -> Option<T>,
+{
+    let value = pop!(hash[key]).ok_or(HashDeErr::KeyNotFound)?;
+    value
+        .into_vec()
+        .ok_or(HashDeErr::InvalidValue)?
+        .into_iter()
+        .map(transform)
+        .collect::<Option<Vec<_>>>()
+        .ok_or(HashDeErr::InvalidValue)
+}
+
+pub fn parse_singular_or_plural<F, T>(
+    hash: &mut Hash,
+    singular: &'static str,
+    plural: &'static str,
+    transform: F,
+) -> Result<Vec<T>, HashDeErr>
+where
+    F: Fn(Yaml) -> Option<T>,
+{
+    if let Ok(value) = parse_key_from_hash(hash, singular, &transform) {
+        return Ok(vec![value]);
+    }
+
+    parse_vec_key_from_hash(hash, plural, &transform)
 }
 
 #[cfg(test)]
